808504a211179ecb599a320c1454e6a1e8bf31d6
Tamara Zoric <t.zoric@vegait.rs>
2022-11-28 16:50:36 +0100

    add initial tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,22 @@ module.exports = {
     './app.vue',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        BLAUW: '#B7E1E0',
+        ZILVER: '#4e4d4b',
+        ZWART: '#1d1d1d',
+        ROZE: '#f9ded7',
+        WIT: '#f7f6f6',
+        GRIJS: '#808080',
+        GOUD: '#ffd700',
+        ROOD: '#a30112',
+        GOREN: '#215e7b',
+      },
+    },
+  },
+  variants: {
+    display: ['group-hover'],
   },
   plugins: [],
 }
