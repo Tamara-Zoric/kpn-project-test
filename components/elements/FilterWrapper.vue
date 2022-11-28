d22d4821ca335077cb6c1a2b4c2d81012d7c6045
Tamara Zoric <t.zoric@vegait.rs>
2022-11-28 16:54:15 +0100

    create initial fiter reusable component

---
+++ components/elements/FilterWrapper.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import Dropdown from '~/components/elements/Dropdown.vue'
+import filters from '~/filters.json'
+
+const handleDropdownChange = (e: Event) => {
+  console.log(e)
+}
+</script>
+
+<template>
+  <div class="flex bg-white mb-5 rounded">
+    <dropdown
+      v-for="(filter, key) in filters.filters"
+      :key="key"
+      :name="key"
+      :options="filter"
+      @change="handleDropdownChange"
+    ></dropdown>
+  </div>
+</template>
